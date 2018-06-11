fb758c8f8cb6852a59d53dee322be227605e6922
Anthony Rioux <you@example.com>
2018-06-11 18:12:12 -0400

    Basic client server functionality is implemented.  The core can poll the agent over the network.

--- agent.py
+++ agent.py
@@ -1,43 +1,98 @@
 
 import subprocess
 import re
+import socket
+#from _thread import *
+import threading
 
 disks = ['sda', 'sdb', 'sdc', 'sdd', 'sde']
 p_errors = re.compile(r'No Errors Logged')
 
 
-print( '\n' * 5 + 20 * '=' + '| Disk Status |' + 20 * '=')
+def disk_status():
+    print('\n' * 5 + 20 * '=' + '| Disk Status |' + 20 * '=')
+    status = '\n' * 5 + 20 * '=' + '| Disk Status |' + 20 * '=' + '\n'
+    for i in disks:
+        output = ""
+        try:
+            output = subprocess.check_output('smartctl -a /dev/' + i, stderr=subprocess.STDOUT, shell=True)
+        except:
+            pass
+        m_errors = None
+        m_errors = p_errors.search(output)
+        if m_errors:
+            print(i + ": OK")
+            status += i + ": OK\n"
+        else:
+            print(i + ": ERROR")
+            status += i + ": ERROR\n"
+    #    print(m_errors.group())
+    return status
 
-for i in disks:
-    output = ""
-    try:
-        output = subprocess.check_output('smartctl -a /dev/' + i, stderr=subprocess.STDOUT, shell=True)
-    except:
-        pass
-    m_errors = None
-    m_errors = p_errors.search(output)
-    if m_errors:
-        print( i + ": OK")
+
+def raid_status():
+    print('\n' * 3 + 20 * '=' + '| Array Status |' + 20 * '=')
+    status = '\n' * 3 + 20 * '=' + '| Array Status |' + 20 * '=' + '\n'
+    RAID_arrays = ['md0']
+    p_array_status = re.compile(r'State : (.*)')
+    for i in RAID_arrays:
+        output = ""
+        try:
+            output = subprocess.check_output('mdadm --detail /dev/' + i, stderr=subprocess.STDOUT, shell=True)
+        except:
+            pass
+        m_array_status = None
+        m_array_status = p_array_status.search(output)
+        if m_array_status:
+            print(m_array_status.group())
+            status += m_array_status.group() + "\n"
+        else:
+            print("Something went wrong... Can't parse mdadm output.  Check it manually.")
+            status += "Something went wrong... Can't parse mdadm output.  Check it manually.\n"
+
+    return status
+
+
+def command_parse(command):
+    command = command.rstrip()
+    print("["+command + "]")
+    if command == "disk-status":
+        output = disk_status()
+    elif command == "raid-status":
+        output = raid_status()
     else:
-        print( i + ": ERROR")
-#    print(m_errors.group())
+        output = "unknown command"
+    return output
 
 
-print( '\n' * 3 + 20 * '=' + '| Array Status |' + 20 * '=' )
+def server_talk(c):
+    while True:
+        data = c.recv(1024)
+        if data:
+            print("received a command")
+            output = command_parse(data)
+            c.send(output + "done - got our data")
+        else:
+            print("client disconnected")
+            break
 
-RAID_arrays = ['md0']
-p_array_status = re.compile(r'State : (.*)')
-for i in RAID_arrays:
-    output = ""
-    try:
-        output = subprocess.check_output('mdadm --detail /dev/' + i, stderr=subprocess.STDOUT, shell=True)
-    except:
-        pass
-    m_array_status = None
-    m_array_status = p_array_status.search(output)
-    if m_array_status:
-        print(m_array_status.group())
-    else:
-        print("something went wrong...")
 
-print('\n' * 4)
+def server():
+    host = ""
+    port = 22000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket bound", port)
+    s.listen(25)    # queue 25 connection requests before refusing requests
+    print("socket listening")
+
+    while True:
+        c, addr = s.accept()    # accept client connection
+        print("Client connected :" + str(addr[0]) + ':' + str(addr[1]))
+        #server_talk(c)
+        threading.Thread(target=server_talk, args=(c,)).start()
+    s.close()
+
+
+if __name__ == "__main__":
+    server()
--- core.py
+++ core.py
@@ -1,9 +1,50 @@
 
-import subprocess
 
-try:
-except:
-    pass
+import sys
+import socket
+import time
+import re
 
-print output
+# grab hosts to manage from mongo
+# select host
+    # poll for status
+# discover disks to monitor
 
+PORT = 22000
+POLL_INTERVAL = 30   # poll every 30 seconds,  might want to set it to 5 minutes
+
+p_end_connection = re.compile(r'done - got our data')
+
+
+def poll_agent(host, checks):
+    status = ""
+    for i in checks:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, PORT))
+        s.send(i.encode('ascii'))
+        while True:
+            status += s.recv(1024)
+            if not status:  # agent disconnected, not normal
+                break
+            m_end = p_end_connection.search(status)
+            if m_end:       # end of message found
+                break       # stop receiving data
+        s.close()
+    return status
+
+
+def poll_loop(host):
+    while True:
+        checks = ['disk-status', 'raid-status']
+        status = poll_agent(host, checks)
+        print(status)
+        time.sleep(POLL_INTERVAL)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+        poll_loop(host)
+    else:
+        print("wrong args ...")
